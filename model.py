57188ebb77d6c842061eefd7c23e52f70b257d35
krenerd <48239275+krenerd@users.noreply.github.com>
2021-01-05 11:24:03 +0900

    Fix model

--- model.py
+++ model.py
@@ -5,11 +5,11 @@ def build_generator(image_size=(64,64),channels=64):
     w,h=image_size
     
     model = tf.keras.Sequential()
-    model.add(layers.Dense(w*h*channels//16, use_bias=False, input_shape=(100,)))
+    model.add(layers.Dense(8*8*channels, use_bias=False, input_shape=(100,)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Reshape((w//8,h//8,channels*4)))
+    model.add(layers.Reshape((8,8,channels*4)))
 
     model.add(layers.Conv2DTranspose(channels*4, 4, strides=(1, 1), padding='same', use_bias=False))
     model.add(layers.BatchNormalization())
@@ -24,12 +24,15 @@ def build_generator(image_size=(64,64),channels=64):
     model.add(layers.LeakyReLU())
     
     model.add(layers.Conv2DTranspose(3, 4, strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    model.add(layers.experimental.preprocessing.Resizing(w,h))
+
     return model
 
 def build_discriminator(image_size=(64,64),channels=64):
     model = tf.keras.Sequential()
-    model.add(layers.Conv2D(channels, 4, strides=(2, 2), padding='same',
-                                     input_shape=image_size+(3,)))
+
+    #model.add(layers.experimental.preprocessing.Resizing(64,64))
+    model.add(layers.Conv2D(channels, 4, strides=(2, 2), padding='same',input_shape=(64,64,3))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
     #model.add(layers.Dropout(0.3))
